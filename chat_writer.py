7ce0a7826d3fcd7b2001cf811222f7b247fec1c4
nstonic <118763806+nstonic@users.noreply.github.com>
2023-06-18 19:50:09 +0400

    Update chat_writer.py

--- chat_writer.py
+++ chat_writer.py
@@ -2,52 +2,101 @@ import argparse
 import asyncio
 import json
 import logging
-from asyncio import StreamWriter, StreamReader
+from asyncio import StreamWriter
 from datetime import datetime
-from typing import NoReturn
+from functools import partial
+from typing import NoReturn, Callable, Optional
 
 from aioconsole import ainput
 from environs import Env
 
 
-async def submit_message(writer: StreamWriter) -> NoReturn:
-    message = await ainput('Введите сообщение:')
+async def submit_message(
+        writer: StreamWriter,
+        message: str
+) -> NoReturn:
     message = message.replace('\n', ' ')
     writer.write(f'{message}\n\n'.encode(errors='ignore'))
     await writer.drain()
     sending_time = datetime.now().strftime('%d.%m.%Y %H:%M')
     logging.debug(f'Sending message [{sending_time}] {message}')
     print('Сообщение отправлено')
+    writer.close()
+    await writer.wait_closed()
+    exit()
 
 
-async def register(reader: StreamReader, writer: StreamWriter) -> None:
-    nickname = await ainput('Введите никнейм для регистрации:')
+async def register(
+        writer: StreamWriter,
+        nickname: str = None
+) -> None:
+    if not nickname:
+        nickname = await ainput('Введите никнейм для регистрации:')
     nickname = nickname.replace('\n', ' ')
     writer.write(f'{nickname}\n'.encode(errors='ignore'))
     await writer.drain()
-    answer = await reader.read(512)
-    token = json.loads(answer.decode(errors='ignore')).get('account_hash')
+
+
+async def authorise(
+        writer: StreamWriter,
+        chat_token: str,
+) -> None:
+    writer.write(f'{chat_token}\n'.encode(errors='ignore'))
+    await writer.drain()
+
+
+async def print_unknown_token() -> None:
+    print('Неизвестный токен.')
+
+
+async def print_new_token(token: str) -> None:
     print(f'Ваш новый токен: {token}\nСохраните его в файле .env')
 
 
-async def authorise(reader: StreamReader, writer: StreamWriter, chat_token: str) -> None:
-    answer = await reader.read(512)
-    if 'Enter your personal hash' in answer.decode(errors='ignore'):
-        writer.write(f'{chat_token}\n'.encode(errors='ignore'))
-        await writer.drain()
-        answer = await reader.read(512)
-        if json.loads(answer.decode(errors='ignore')) is None:
-            await reader.read(512)
-            print('Неизвестный токен.', end='')
-            await register(reader, writer)
+def get_action_func(
+        answer: str,
+        writer: StreamWriter,
+        message: str,
+        chat_token: str,
+        nickname: str
+) -> Optional[Callable]:
+    if 'Enter preferred nickname below' in answer:
+        return partial(register, writer=writer, nickname=nickname)
+    if 'Enter your personal hash' in answer:
+        return partial(authorise, writer=writer, chat_token=chat_token)
+    if 'Post your message below' in answer:
+        return partial(submit_message, writer=writer, message=message)
+
+    try:
+        answer_obj = json.loads(answer)
+    except json.decoder.JSONDecodeError:
+        return
+    else:
+        if answer_obj is None:
+            return print_unknown_token
+        elif token := answer_obj.get('account_hash'):
+            return partial(print_new_token, token=token)
 
 
-async def start_chat(host: str, port: int, chat_token: str) -> None:
+async def send_message(
+        host: str,
+        port: int,
+        chat_token: str,
+        message: str,
+        nickname: str
+) -> None:
     reader, writer = await asyncio.open_connection(host, port)
-    await authorise(reader, writer, chat_token)
     try:
         while True:
-            await submit_message(writer)
+            answer = await reader.read(512)
+            if action := get_action_func(
+                    answer.decode(errors='ignore'),
+                    writer,
+                    message,
+                    chat_token,
+                    nickname
+            ):
+                await action()
     finally:
         writer.close()
         await writer.wait_closed()
@@ -58,6 +107,15 @@ def main():
     env.read_env()
 
     parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'message',
+        help='Message for sending'
+    )
+    parser.add_argument(
+        '--nickname',
+        default=env('NICKNAME', default=None),
+        help='Nickname for registration'
+    )
     parser.add_argument(
         '--host',
         default=env('HOST', 'minechat.dvmn.org'),
@@ -90,10 +148,12 @@ def main():
         filename=args.log_filename
     )
 
-    asyncio.run(start_chat(
+    asyncio.run(send_message(
         host=args.host,
         port=args.port,
-        chat_token=env('CHAT_TOKEN')
+        chat_token=env('CHAT_TOKEN', default=None),
+        message=args.message,
+        nickname=args.nickname
     ))
 
 
